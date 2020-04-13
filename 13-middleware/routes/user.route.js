ec5279de0eb3d79d2d461ede338e57b4191cd3c9
detectiveno2 <nhatthien185@gmail.com>
2020-04-13 15:47:35 +0700

    13-middleware

---
+++ 13-middleware/routes/user.route.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var controller = require('./../controller/users.controller.js');
+var validation = require('./../middleware/validation/users.validation.js');
+
+router.get('', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.post('/create', validation.postCreate, controller.postCreate);
+
+router.get('/:id', controller.view);
+
+module.exports = router;
